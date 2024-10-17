b76e2a4adf3a086685e6660916ca6856c4d8e79d
mikeneko1224 <amiba725@gmail.com>
2024-10-17 21:50:05 +0900

    Add search result page

--- ryouri/src/App.js
+++ ryouri/src/App.js
@@ -2,13 +2,15 @@ import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import Detail from "./detail/detail.js";
+import Result from "./result/result.js";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route path="/" element={<Home />} /> {/* ホームコンポーネント */}
+          <Route path="/" element={<Home />} />
+          <Route path="/result" element={<Result />} />
           <Route path="/detail" element={<Detail />} />
         </Routes>
       </div>
@@ -18,9 +20,14 @@ function App() {
 
 function Home() {
   return (
-    <div className="change_page">
-      <Link to="/detail">Detailページへ</Link>
-    </div>
+    <>
+      <div className="change_page">
+        <Link to="/result">Resultページへ</Link>
+      </div>
+      <div className="change_page">
+        <Link to="/detail">Detailページへ</Link>
+      </div>
+    </>
   );
 }
 
